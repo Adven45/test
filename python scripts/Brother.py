9dfd479b7e45e2febdd0fc14986922fd24a92142
Adven45 <srinithsr10@gmail.com>
2025-06-05 11:46:08 +0530

    learning how to commit

--- python scripts/Brother.py
+++ python scripts/Brother.py
@@ -25,7 +25,7 @@ try:
     driver.find_element(By.XPATH, '//input[@value="Continue"]').click()
 
     # URI entry
-    sn = "172.16.158.89/MHJ-BS-BARCODE-RTPCR01"  # Example serial number or name
+    sn = "172.16.163.122/MHG-LAB-BARCODE5"  # Example serial number or name
     lpd_input = wait.until(EC.presence_of_element_located((By.NAME, "DEVICE_URI")))
     lpd_input.clear()
     lpd_input.send_keys(f"lpd://{sn}")
@@ -33,19 +33,18 @@ try:
 
     # Printer name, info, location
     wait.until(EC.presence_of_element_located((By.NAME, "PRINTER_NAME"))).send_keys(sn.split("/")[1])
-    #driver.find_element(By.NAME, "PRINTER_INFO").send_keys("TVS")
-    driver.find_element(By.NAME, "PRINTER_INFO").send_keys("Zenpert 4T520")
+    driver.find_element(By.NAME, "PRINTER_INFO").send_keys("Brother HL-L2360D Printer")
     driver.find_element(By.NAME, "PRINTER_LOCATION").send_keys(sn.split("/")[1])
     driver.find_element(By.XPATH, '//input[@value="Continue"]').click()
 
     # Select make
     select_make = wait.until(EC.presence_of_element_located((By.NAME, "PPD_MAKE")))
-    Select(select_make).select_by_value("Generic")
+    Select(select_make).select_by_value("Brother")
     driver.find_element(By.XPATH, '//input[@value="Continue"]').click()
 
     # Select model
     select_model = wait.until(EC.presence_of_element_located((By.NAME, "PPD_NAME")))
-    Select(select_model).select_by_value("lsb/usr/cupsfilters/TT033-50.ppd")
+    Select(select_model).select_by_value("foomatic:Brother-HL-2060-pxlmono.ppd")
     add_printer_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//input[@value="Add Printer"]')))
     add_printer_button.click()
     driver.find_element(By.XPATH,'//input[@value="Set Default Options"]').click()
--- python scripts/Myversion.py
+++ python scripts/Myversion.py
@@ -26,20 +26,26 @@ def setup_printer(sn,url):
     driver.find_element(By.XPATH, '//input[@value="Continue"]').click()
     wait.until(EC.presence_of_element_located((By.NAME, "PRINTER_NAME"))).send_keys(sn.split("/")[1])
     driver.find_element(By.NAME, "PRINTER_INFO").send_keys("HP LaserJet Pro M202dw")
+    #driver.find_element(By.NAME, "PRINTER_INFO").send_keys("Zebra")
     driver.find_element(By.NAME, "PRINTER_LOCATION").send_keys(sn.split("/")[1])
     driver.find_element(By.XPATH, '//input[@value="Continue"]').click()
     Select_make = wait.until(EC.presence_of_element_located((By.NAME, "PPD_MAKE")))
     Select(Select_make).select_by_value("HP")
+   # Select(Select_make).select_by_value("Generic")
     wait.until(EC.element_to_be_clickable((By.XPATH, '//input[@value="Continue"]'))).click()
     select_model = wait.until(EC.presence_of_element_located((By.NAME, "PPD_NAME")))
     Select(select_model).select_by_value("pxlmono.ppd")
+    #Select(select_model).select_by_value("lsb/usr/cupsfilters/TT033-50.ppd")
     add_printer_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//input[@value="Add Printer"]')))
     add_printer_button.click()
     wait.until(EC.element_to_be_clickable((By.XPATH,'//input[@value="Set Default Options"]'))).click()
 #setup_printer("10.91.101.168/MBO-GF-HEALTHCHKP","http://172.25.36.36:631")
 #list=["http://172.25.36.36:631"]
 list=["http://172.16.242.127:631","http://172.16.242.128:631","http://172.16.242.129:631","http://172.16.242.130:631","http://172.16.242.131:631","http://172.16.242.132:631","http://172.16.242.133:631","http://172.16.242.134:631"]
-
+ADD_PRINTERS=["10.92.62.47/MKB-1F-OPD6"]
 for ip in list:
-    setup_printer("10.91.37.36/MGD-GF-ER",ip)
+    #setup_printer("10.92.62.69/MKB-2F-ICU3",ip)
+    for addprint in ADD_PRINTERS:
+        setup_printer(addprint,ip)
+        print(addprint,"is done",ip)
 
